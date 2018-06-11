1b1e220f71a11037a46bae922de58bcaaf233cbe
Hervé Ah-Leung <herve.ahleung@gmail.com>
2018-06-11 23:05:01 +0100

    Codewars: Rot13 #refactor

--- codewars/rot13.ex
+++ codewars/rot13.ex
@@ -6,37 +6,21 @@ defmodule Encryptor do
   def rot13(string) do
     alphabet = for n <- ?a..?z, do: <<n::utf8>>
     rot13_alphabet = Enum.drop(alphabet, 13) ++ Enum.take(alphabet, 13)
-    translation_table = Enum.zip(alphabet, rot13_alphabet) |> Enum.into(%{})
+
+    build_table = fn {k, v}, table ->
+      Enum.into(table, %{k => v, String.upcase(k) => String.upcase(v)})
+    end
+
+    translation_table = Enum.zip(alphabet, rot13_alphabet) |> Enum.reduce(%{}, build_table)
+
+    translate = fn char -> Map.get(translation_table, char, char) end
 
     String.codepoints(string)
-    |> Enum.map(&translate(&1, translation_table))
+    |> Enum.map(translate)
     |> List.to_string()
-  end
-
-  defp translate(char, translation_table) do
-    do_translate(char, Regex.match?(~r/[a-zA-Z]/, char), translation_table)
-  end
-
-  defp do_translate(char, false, _), do: char
-
-  defp do_translate(char, true, translation_table) do
-    case Regex.match?(~r/^[A-Z]+$/, char) do
-      false -> translation_table[char]
-      _ -> translation_table[String.downcase(char)] |> String.upcase()
-    end
   end
 end
 
 # assert "Grfg" == rot13("Test")
 Encryptor.rot13("C[\\V69NTP@<G>U?8=AE^ROD0Ya]`3KM5QI_SXHL:;B427JZW1F")
 |> IO.inspect()
-
-# On the Web, cleaner with default the Map.get/3
-  # def rot13(string) do
-  #   abc = String.codepoints("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-  #   mno = String.codepoints("nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM")
-
-  #   transform = Enum.zip(abc, mno) |> Map.new(fn {k, v} -> {k, v} end)
-
-  #   String.codepoints(string) |> Enum.map(fn c -> Map.get(transform, c, c) end) |> Enum.join
-  # end
